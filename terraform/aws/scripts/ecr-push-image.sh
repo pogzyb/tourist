054649afda494011b018ccd5f95adeee20a3cf4b
Joe Obarzanek <pogzyb@umich.edu>
2024-08-02 19:19:34 -0400

    Fix ecr push script

--- terraform/aws/scripts/ecr-push-image.sh
+++ terraform/aws/scripts/ecr-push-image.sh
@@ -1,7 +1,6 @@
 # The tourist image needs to be in AWS ECR in the user's AWS account, 
 # so we pull it from github, tag it, and push it to ECR.
-# docker pull ghcr.io/pogzyb/tourist:${TOURIST_VERSION}
-docker pull ghcr.io/pogzyb/czdsdump:0.1.0
+docker pull ghcr.io/pogzyb/tourist:${TOURIST_VERSION}
 docker login -u AWS -p $(aws ecr get-login-password --region $REGION) $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
-docker tag ghcr.io/pogzyb/czdsdump:0.1.0 $REPO_URL:$TOURIST_VERSION
+docker tag ghcr.io/pogzyb/tourist:${TOURIST_VERSION} $REPO_URL:$TOURIST_VERSION
 docker push $REPO_URL:$TOURIST_VERSION
